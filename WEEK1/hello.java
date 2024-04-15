a714de17cf49ab2f75a633c4054cc524db28abf5
malladeepakj@gmail.com <malladeepakj@gmail.com>
2024-04-15 12:09:41 +0545

    final task 1

--- WEEK1/hello.java
+++ WEEK1/hello.java
@@ -1,7 +1,120 @@
-package WEEK1;
-public class hello {
+// 1. Write a program that prints Hello World! to the console. Save the program as Hello.java.
+// package WEEK1;
+// public class hello {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
+//     public static void main(String[] args) {
+//         System.out.println("Hello World");
+//     }
+// }
+
+
+// 2. Extend/Modify the above program to print two additional lines:
+
+// This is my first program
+
+// I am on module ST4003CEM
+
+// System.out.print (not println) and produces the same output.
+// package WEEK1;
+
+//  public class hello {
+
+//     public static void main(String[] args) {
+//         System.out.print("This is my first program ");
+//         System.out.print("I am on module ST4003CEM");
+//     }
+// }
+
+
+
+
+// 3. Write a program that prints the following output:
+
+// A "quoted" String is
+
+// 'much' better if you learn
+
+// the rules of "escape sequences."
+
+// Also, "" represents an empty
+
+// String. Don't forget: use \"
+
+// instead of " !
+
+// '' is not the same as "
+
+// package WEEK1;
+
+//  public class hello {
+//     public static void main(String[] args) {
+//         System.out.println("A\"quoted\" string is ");
+//         System.out.println("\'much'better if you learn");
+//         System.out.println("the rules of \"escape sequences,\"");
+//         System.out.println("Alos, \"\" represents an empty");
+//         System.out.println("Stirng. Don't Forget : use\\\"");
+//         System.out.println("instead of\"!");
+//         System.out.println("''is not the same as \"i java");
+//     }
+
+    
+// }
+
+
+
+
+
+// 4. WAP that prints the following pattern:
+
+// *
+
+// **
+
+// ***
+
+// ****
+
+// package WEEK1;
+
+// public class hello {
+//     public static void main(String[] args) {
+//         System.out.println("*");
+//         System.out.println("**");
+//         System.out.println("***");
+//         System.out.println("****");
+//         System.out.println("********");
+//         System.out.println("********");
+//         System.out.println("********");
+//         System.out.println("********");
+//         System.out.println("********");
+//         System.out.println("********");
+//     }
+
+    
+// }
+
+
+// 6. WAP that prints the following output:
+
+// A well-formed Java program has a main method with { and } braces.
+
+
+
+// A System.out.println statement has ( and ) and usually a String that starts and ends with a " character.
+
+// (But we type \" instead!)
+
+// What is the difference between
+
+// a ' and a "?  Or between a " and a \"?6. WAP that prints the following output:
+
+
+//  package WEEK1;
+
+//  public class hello {
+//  public static void main(String[] args) {
+//     System.out.println("A well-formed Java program has a main method with { and } braces.");
+//     System.out.println("A System.out.println statement has ( and ) and usually a String that starts and ends with a ");
+//  }
+    
+// }
