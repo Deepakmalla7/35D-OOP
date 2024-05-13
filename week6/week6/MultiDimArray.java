295147266f1275b94b4f5732914bceba2bfd58e7
malladeepakj@gmail.com <malladeepakj@gmail.com>
2024-05-13 13:32:27 +0545

    All

---
+++ week6/week6/MultiDimArray.java
@@ -0,0 +1,26 @@
+package week6;
+public class MultiDimArray {
+
+    public static void main(String[] args) {
+        // creat n-domension array add number of square bracked
+        // 1D Array
+        int[][][ ]threeDarray;
+        int twoDArray[][];
+
+        // Allocating memory\size
+        twoDArray = new int[3][5];
+        // first size 3 represent how many inner array
+        // second size 5 present how many value in inner array
+
+        [
+            // [0,0,0,0],
+            // [0,0,0,0],
+            // [0,0,0,0]
+
+        ]
+        // /
+
+        // To make the first inner array use index
+        twoDArray[0];
+    }
+}
